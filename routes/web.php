579c5accf1cafa362ff97c39580bcf81d1316295
omd34 <danielotomo34@gmail.com>
2024-12-11 16:06:37 +0100

    MAJ des routes comme sous domaine

--- routes/web.php
+++ routes/web.php
@@ -19,26 +19,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('index');
-Route::prefix('/biens')->controller(ControllersPropertyController::class)->name('property.')->group(function () {
+Route::get('/legendeimmobiliere', [HomeController::class, 'index'])->name('index');
+Route::prefix('/legendeimmobiliere/biens')->controller(ControllersPropertyController::class)->name('property.')->group(function () {
     $idRegex = '[0-9]+';
     $slugRegex = '[0-9a-z\-]+';
     Route::get('/', 'index')->name('index');
-    Route::get('/{slug}/{property}', 'show')->name('show')->where([
+    Route::get('/legendeimmobiliere/{slug}/{property}', 'show')->name('show')->where([
         'slug' => $slugRegex,
         'property' => $idRegex
     ]);
-    Route::post('/{property}/contact', 'contact')->name('contact')->where([
+    Route::post('/legendeimmobiliere/{property}/contact', 'contact')->name('contact')->where([
         'property' => $idRegex
     ]);
 });
 
-Route::get('/admin/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
-Route::post('/admin/login', [AuthController::class, 'authentificate']);
-Route::delete('/admin/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/legendeimmobiliere/admin/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/legendeimmobiliere/admin/login', [AuthController::class, 'authentificate']);
+Route::delete('/legendeimmobiliere/admin/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
-    Route::get('/', [AdminHomeController::class, 'index'])->name('index');
-    Route::resource('property', PropertyController::class)->except('show');
-    Route::resource('option', OptionController::class)->except('show');
+Route::prefix('/legendeimmobiliere/admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('/legendeimmobiliere/', [AdminHomeController::class, 'index'])->name('index');
+    Route::resource('legendeimmobiliere/property', PropertyController::class)->except('show');
+    Route::resource('legendeimmobiliere/option', OptionController::class)->except('show');
 });
